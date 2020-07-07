dba3b776cf192c3faa7e60f572484fcad1a37f46
Neo Chen <43288434+NeoKokuxz@users.noreply.github.com>
2020-07-06 22:17:59 -0400

    First RESTful API initial update

---
+++ rest-endpoint/src/main/java/com/neokokuxz/restendpoint/RestEndpointApplication.java
@@ -0,0 +1,13 @@
+package com.neokokuxz.restendpoint;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestEndpointApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestEndpointApplication.class, args);
+	}
+
+}
